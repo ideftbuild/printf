4e90c369ee18bf4df9b7b30173868a62540a6013
akanSWE <a.akanSWE@gamil.com>
2023-10-13 22:47:24 +0100

    add: Implemented the first phase of _printf function
    The current state of printf function can only collect integral types
    that is '%d, %i, %x, %X, %u', prints out non formatted characters, and
    also defined the struct that maps this format specifier to it corresponding
    function, the algorithm of this _printf function makes it possible to accept
    any number of specified format specifier, if any specifier and it function is
    implemented just add it to the structure.

---
+++ printf.c
@@ -0,0 +1,70 @@
+#include "printf.h"
+
+/**
+ * _printf - Format and print data
+ *
+ * @format: Formated string
+ *
+ * Return: The number of bytes printed
+ */
+int _printf(const char *format, ...)
+{
+	int noOfBytes;
+	va_list args;
+
+	va_start(args, format); /* Initialize the list */
+
+	noOfBytes = 0;
+
+	noOfBytes += handle_format_strings(format, args);
+
+	va_end(args);
+
+	return (noOfBytes); /* Total number of bytes printed */
+}
+
+/**
+ * handle_format_strings - handle the printing of formatted
+ * string
+ *
+ * @format: The format string
+ *
+ * @args: The argument list
+ *
+ * Return: The number of bytes printed
+ */
+int handle_format_strings(const char *format, va_list args)
+{
+	int noOfBytes, i;
+
+	s_H specifierHandler[] = {
+		{'d', handle_integers},
+		{'i', handle_integers},
+		{'x', handle_lowerHexi},
+		{'X', handle_upperHexi},
+		{'u', handle_unsigned},
+		{'\0', NULL}
+	};
+
+	noOfBytes = 0;
+	while (format && *format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			noOfBytes++, format++;
+			continue;
+		}
+		format++; /* Move past '%' */
+		i = 0;
+		while (specifierHandler[i].formatSpecifier != '\0')
+		{
+			if (*format == specifierHandler[i].formatSpecifier)
+				noOfBytes += specifierHandler[i].handlerFunction_ptr(args);
+			i++;
+		}
+
+		format++;
+	}
+	return (noOfBytes); /* Total number of bytes printed */
+}
