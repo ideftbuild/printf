a8d3e4754e3a823f3776812482c40afae7177abf
akanSWE <a.akanSWE@gamil.com>
2023-10-17 20:08:57 +0100

    add: Add new test, checking the output of the _printf function, no bugs for now

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "printf.h"
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -10,18 +11,34 @@ int main(void)
 {
 	int noOfBytes;
 
-	/* Test 1 */
-	noOfBytes = _printf("akan %u %+d %#x it working fine\n", 23, 23, 23);
-	printf("%d\n", noOfBytes);
 
-	noOfBytes = printf("Loni %u %+d %#x it working fine\n", 23, 23, 23);
-	printf("%d\n", noOfBytes);
+	/* Test 1 */
+	noOfBytes = _printf("I love number %d because it %x in hexi and %u in unsigned\nhurray it %d\n, my favourite binary is %b\n test last = %#x\n", 23, 23, 23, 2023, 99, 52);
+
+	_printf("noOfBytes = %d\n", noOfBytes);
+
+	 /* noOfBytes = printf("I love number %d because it %x in hexi and %u in unsigned\nhurray it %d\n, my favourite binary is %b\n test last = %#x\n", 23, 23, 23, 2023, 99, 52);
+
+	printf("noOfBytes = %d\n", noOfBytes); */
+
 
 	/* Test 2 */
-	noOfBytes = _printf("%x %x %x", -1024, 1024, 292929);
+	noOfBytes = _printf("What programming withoug binaries %b, lol\n", 2023);
+
+	printf("noOfBytes = %d\n", noOfBytes);
+
+	/* Test 3 */
+	noOfBytes = _printf("akan %u %+d %#x it working fine!!\n", 23, 23, 23);
 	printf("%d\n", noOfBytes);
 
-	noOfBytes = printf("%x %x %x", -1024, 1024, 292929);
+	noOfBytes = printf("akan %u %+d %#x it working fine!!\n", 23, 23, 23);
+	printf("%d\n", noOfBytes);
+
+	/* Test 4 */
+	noOfBytes = _printf("%x %x %x\n", -1024, 1024, 292929);
+	printf("%d\n", noOfBytes);
+
+	noOfBytes = printf("%x %x %x\n", -1024, 1024, 292929);
 	printf("%d\n", noOfBytes);
 
 	return (0);
